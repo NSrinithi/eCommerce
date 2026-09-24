4aca0d087f2953b19384ccefc67928c8264c9307
Srinithi <srinithinithiyanantham@gmail.com>
2026-09-24 21:41:34 +0530

    admin orders-2

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -14,7 +14,7 @@ import { AdminDashboardPage } from '../pages/admin/AdminDashboardPage.jsx';
 import { AddProductPage } from '../pages/admin/AddProductPage.jsx';
 import { AdminProductsPage } from '../pages/admin/AdminProductsPage.jsx';
 import { EditProductPage } from '../pages/admin/EditProductPage.jsx';
-import { AdminOrderPage } from '../pages/admin/ADminOrderPage.jsx';
+import { AdminOrderPage } from '../pages/admin/AdminOrderPage.jsx';
 import { OrdersPage } from '../pages/orders/OrdersPage.jsx';
 import { CheckOutPage } from '../pages/checkout/CheckOutPage.jsx';
 import { NotFoundPage } from '../pages/NotFoundPage.jsx';
