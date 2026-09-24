f181b13ce5340fa41333e11fce7308d0484ac3e5
Srinithi <srinithinithiyanantham@gmail.com>
2026-09-24 12:54:57 +0530

    changed path-3

--- frontend/src/pages/auth/LoginPage.jsx
+++ frontend/src/pages/auth/LoginPage.jsx
@@ -24,11 +24,11 @@ export function LoginPage() {
     try {
       await login(form);
       const from = location.state?.from;
-      if (typeof from === 'string' && from.startsWith('/') && !from.startsWith('//')) {
-        navigate(from, { replace: true });
-        return;
-      }
-
+      // if (typeof from === 'string' && from.startsWith('/') && !from.startsWith('//')) {
+      //   navigate(from, { replace: true });
+      //   return;
+      // }
+      console.log(user?.role);
       if (user?.role === 'ADMIN') {
         navigate('/admin/dashboard', { replace: true });
       } else {
