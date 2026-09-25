bd70cc35091e880031e7cd76eddd2c68c1e86f4c
Srinithi <srinithinithiyanantham@gmail.com>
2026-09-26 01:17:51 +0530

    added pagination-19

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -74,7 +74,7 @@ export function Header({ onMenu }) {
           </span>
         </Link>
         <Link to="/wishlist">
-          <i class="fa-regular fa-heart" style="color: rgb(0, 0, 0);"></i>
+          <i className="fa-regular fa-heart" style={{color: "rgb(0, 0, 0)"}}></i>
         </Link>
         <span className="header-divider" />
 
